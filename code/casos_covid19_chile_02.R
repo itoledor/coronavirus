a249394710ee6aad603f05e1883469d23fba8cd1
itoledor <nacho.toledo@gmail.com>
2020-03-20 00:29:01 -0300

    bases update

--- code/casos_covid19_chile_02.R
+++ code/casos_covid19_chile_02.R
@@ -11,3 +11,21 @@ covid19_chile <- covid19_chile %>% tail(-1)
 covid19_chile <- covid19_chile %>% mutate(fecha = Sys.Date())
 
 write_csv2(covid19_chile, here("data", paste0("covid19_chile_",Sys.Date(),".csv")))
+
+covid19_chile <- tibble(file_name = dir(here("data")))
+covid19_chile <- covid19_chile %>% filter(file_name %>% str_ends('.csv'))
+covid19_chile <- covid19_chile %>% mutate(data = here("data", file_name) %>% map(read_csv2, col_types = 'cddc'))
+covid19_chile <- covid19_chile %>% unnest(data)
+
+covid19_chile_gg <- covid19_chile    %>% filter(region == 'Total')
+covid19_chile_gg <- covid19_chile_gg %>% ggplot(aes(x = fecha, y = casos_totales, color = region))
+covid19_chile_gg <- covid19_chile_gg %+% geom_point(size = 4)
+covid19_chile_gg <- covid19_chile_gg %+% scale_fill_viridis_d()
+covid19_chile_gg <- covid19_chile_gg %+% labs(title = "COVID19 Casos confirmados en Chile")
+covid19_chile_gg <- covid19_chile_gg %+% theme_minimal()
+covid19_chile_gg <- covid19_chile_gg %+% theme(axis.text.x = element_text(angle = 45, hjust = 1))
+covid19_chile_gg
+
+ggsave(here("figs",paste0("covid18_chile_",Sys.Date(),".png")), width = 5, height = 4)
+
+
